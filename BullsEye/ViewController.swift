0aec8a77183f54071b3fe760f4483a45acc8a61f
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2019-03-09 19:18:39 +0100

    Init

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -1,20 +1,67 @@
-//
-//  ViewController.swift
-//  BullsEye
-//
-//  Created by Oskar Kaminski on 08/03/2019.
-//  Copyright © 2019 Oskar Kaminski. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    var round: Int = 1
+    var score: Int = 0
+    
+    @IBOutlet weak var outletSlider: UISlider!
+    @IBOutlet weak var outletRound: UILabel!
+    @IBOutlet weak var outletScore: UILabel!
+    @IBOutlet weak var outletTarget: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+    }
+    
+    @IBAction func onSliderMove(){
+        let roundedValue = outletSlider.value.rounded()
+        currentValue = Int(roundedValue)
     }
 
-
+    @IBAction func onHitMe(){
+//        var miss = targetValue - currentValue
+//        var message = ""
+//        if(miss < 0){
+//            miss = -miss
+//        }
+//        switch miss {
+//        case Bool(miss == 0):
+//            message = "Excelent! 200 points"
+//        case Bool(miss < 5):
+//            message = "Good! \(200 / 5 * miss) points"
+//        default:
+//            message = "Try next time"
+//        }
+        
+        let alert = UIAlertController(
+            title: "Hello, Oskar",
+            message: "The current value is \(currentValue)",
+            preferredStyle: .alert
+        )
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        startNewRound()
+    }
+    
+    @IBAction func restartGame(){
+        round = 1
+        score = 0
+        startNewRound()
+    }
+    
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        outletTarget.text = "\(targetValue)"
+        currentValue = 50
+        outletSlider.value = Float(currentValue)
+        outletRound.text = "\(round)"
+        outletScore.text = "\(score)"
+    }
 }
-
